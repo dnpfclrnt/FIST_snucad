aa2e37bd8353e0794964a247771acd4b9a2f4b30
dnpfclrnt <dnpfclrnt@gmail.com>
2022-11-30 03:30:17 +0900

    XGBoost model constructed

---
+++ RunParser/RunParser.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+
+class RunParser:
+    def __init__(self, result_path: str):
+        self.jspn_path = None
+        if not os.path.isdir(result_path):
+            os.mkdir(result_path)
+        trial = 0
+        self.result_path = os.path.join(result_path, str(trial))
+        while not os.path.isdir(self.result_path):
+            trial += 1
+            self.result_path = os.path.join(result_path, str(trial))
+        os.mkdir(self.result_path)
+        name = "fist.json"
+        self.json_path = os.path.join(self.result_path, name)
+
+    def update_cluster(self, cluster: list, ppa: dict):
+        cur_result = self.result()
+        for sample in cluster:
+            if tuple(sample) not in cur_result.keys():
+                cur_result[tuple(sample)] = ppa
+        with open(self.result_path, "w") as f:
+            json.dump(cur_result, f)
+
+    def update(self, sample: list, ppa: dict):
+        with open(self.result_path, "r") as f:
+            cur_result = json.load(f)
+        if tuple(sample) not in cur_result.keys():
+            cur_result[tuple(sample)] = ppa
+            with open(self.result_path, "w") as f:
+                json.dump(cur_result, f)
+        return cur_result
+
+    def result(self):
+        with open(self.result_path, "r") as f:
+            result = json.load(f)
+        return result
---
+++ RunParser/__init__.py
@@ -0,0 +1,1 @@
+from .RunParser import RunParser
