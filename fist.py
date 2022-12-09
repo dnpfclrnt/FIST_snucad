f15faa6bfa877636fb8a9de5aab039b2300e4d79
dnpfclrnt <dnpfclrnt@gmail.com>
2022-12-09 14:16:25 +0900

    TEST

--- fist.py
+++ fist.py
@@ -325,7 +325,7 @@ if __name__ == "__main__":
                 param_setup_json="assets/setup.json", num_important_feature=5,
                 result_dir="result")
     model_less_dict = fist.model_less(1)
-    fist.exploit(2)
-    fist.explore(3)
+    fist.exploit(1)
+    fist.explore(1)
     fist.write_result()
 
