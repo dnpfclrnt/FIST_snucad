42528391e45dcae337279f1b522e477b1997c9cb
dnpfclrnt <dnpfclrnt@gmail.com>
2022-12-19 18:13:28 +0900

    DEBUG

--- fist.py
+++ fist.py
@@ -367,6 +367,7 @@ if __name__ == "__main__":
                 transfer_design=None, tune_target="all",
                 param_setup_json="assets/setup.json", num_important_feature=5,
                 result_dir="result")
+    print("Model-less")
     model_less_dict = fist.model_less(1)
     fist.exploit(1)
     fist.explore(1)
